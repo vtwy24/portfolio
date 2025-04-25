d45de34b5cc3239ed8c8af858001a07072bee9f2
Victoria Yeung <victoriayeung24@gmail.com>
2025-04-26 00:38:05 +0100

    svg project fixed and structure formated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>Add Contact Form (Email)</p>
+      <p>add contact form (Email)/js.form?</p>
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import kuromiSvg from '../assets/kuromi-svg.png';
+
+import './Projects.css';
 
 export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <p>Here are your projects…</p>
-      <a href="https://endearing-blancmange-7d9723.netlify.app/" target="_blank" rel="noopener noreferrer" />
-        <img src="https://endearing-blancmange-7d9723.netlify.app/" alt="SVG project preview" className="project-preview" />
+      <p>add projects…</p>
+
+      <div className="project-item">
+        <div className="project-preview">
+          <a
+            href="https://endearing-blancmange-7d9723.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={kuromiSvg}
+              alt="SVG project preview"
+              width="300px"
+            />
+          </a>
+        </div>
+
+        <div className="project-description">
+          <h2>Kuromi x Baku SVG</h2>
+          <p>
+            I created a SVG animation of the Sanrio character Kuromi using HTML and CSS.
+            As it looked a bit plain at first with just Kuromi by herself, I added her
+            companion Baku and skill images to fill the background.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
